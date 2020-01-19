9056f9b48d027db144b8102f1e26d13e86c56bf8
Retard <korri.13inbox.ru>
2020-01-19 07:32:29 +0300

    start with firebase

---
+++ Erectus/aaaaa.swift
@@ -0,0 +1,30 @@
+//
+//  aaaaa.swift
+//  Erectus
+//
+//  Created by Alexey on 19/01/2020.
+//  Copyright © 2020 Alexey. All rights reserved.
+//
+
+import UIKit
+
+class aaaaa: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
